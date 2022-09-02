b69ee4e5724b183e11afd52fd9377a7e2519ae0c
Nowsath04 <110928124+Nowsath04@users.noreply.github.com>
2022-09-02 11:05:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+var L1=label("label","for","firstname","firstame");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","firstname","id","firstname");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+
+
+
+//label function
+function label(tagname,attrname,attrvalue,content){
+var ele=document.createElement(tagname);
+ ele.setAttribute(attrname,attrvalue);
+ ele.innerHTML=content;
+ return ele;
+ //<label for="email">Email</label>
+ //<div class="container">this is container</div>
+}
+//linebreaker
+function linebreaker(element){
+var ele=document.createElement(element);
+return ele;
+}
+
+//inputelement
+function inputelement(tagname,attrname,attrvalue,attrname1,attrvalue1){
+var ele=document.createElement(tagname);
+ele.setAttribute(attrname,attrvalue);
+ele.setAttribute(attrname1,attrvalue1);
+return ele;
+
+}
+var L1=label("label","for","middlename","Middlename");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","middlename","id","middlename");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+
+
+
+//label function
+function label(tagname,attrname,attrvalue,content){
+var ele=document.createElement(tagname);
+ ele.setAttribute(attrname,attrvalue);
+ ele.innerHTML=content;
+ return ele;
+ //<label for="email">Email</label>
+ //<div class="container">this is container</div>
+}
+//linebreaker
+function linebreaker(element){
+var ele=document.createElement(element);
+return ele;
+}
+
+//inputelement
+function inputelement(tagname,attrname,attrvalue,attrname1,attrvalue1){
+var ele=document.createElement(tagname);
+ele.setAttribute(attrname,attrvalue);
+ele.setAttribute(attrname1,attrvalue1);
+return ele;
+
+}
+var L1=label("label","for","lastname","Lastname");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","lastname","id","lastname");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+
+
+
+//label function
+function label(tagname,attrname,attrvalue,content){
+var ele=document.createElement(tagname);
+ ele.setAttribute(attrname,attrvalue);
+ ele.innerHTML=content;
+ return ele;
+ //<label for="email">Email</label>
+ //<div class="container">this is container</div>
+}
+//linebreaker
+function linebreaker(element){
+var ele=document.createElement(element);
+return ele;
+}
+
+//inputelement
+function inputelement(tagname,attrname,attrvalue,attrname1,attrvalue1){
+var ele=document.createElement(tagname);
+ele.setAttribute(attrname,attrvalue);
+ele.setAttribute(attrname1,attrvalue1);
+return ele;
+
+}
+var L1=label("label","for","email","Email");
+var br1=linebreaker("br");
+var I1=inputelement("input","type","email","id","email");
+var br2=linebreaker("br");
+document.body.append(L1,br1,I1,br2);
+
+
+
+
+//label function
+function label(tagname,attrname,attrvalue,content){
+var ele=document.createElement(tagname);
+ ele.setAttribute(attrname,attrvalue);
+ ele.innerHTML=content;
+ return ele;
+ //<label for="email">Email</label>
+ //<div class="container">this is container</div>
+}
+//linebreaker
+function linebreaker(element){
+var ele=document.createElement(element);
+return ele;
+}
+
+//inputelement
+function inputelement(tagname,attrname,attrvalue,attrname1,attrvalue1){
+var ele=document.createElement(tagname);
+ele.setAttribute(attrname,attrvalue);
+ele.setAttribute(attrname1,attrvalue1);
+return ele;
+
+}
